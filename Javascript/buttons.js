7ebefb970882ae42d9e9b1014294b0e5539c86f2
cpmcnally <frostyeel@gmail.com>
2016-01-07 19:59:24 -0800

    nav_bar positioning and reactive buttons

--- Javascript/buttons.js
+++ Javascript/buttons.js
@@ -10,15 +10,28 @@
 	button_maker.button_off_color = "#CDC9C9";
 	button_maker.text_tag = "_text"
 
+	button_maker.activate = function(button) {
+		button.attr("on",true);
+		button.select("rect").attr("fill", button_maker.button_on_color);
+		button.classed("activebutton", true);
+		button.selectAll("text").attr("opacity",1);
+	}
+	
+	button_maker.deactivate = function(button) {
+		button.attr("on",false);
+		button.select("rect").attr("fill", button_maker.button_off_color);
+		button.classed("activebutton", false);
+		button.selectAll("text").attr("opacity",0.5);
+	}
+	
 	button_maker.add_rect_button = function(element, id, x, y, width, height, fill, text, on_click){
 
-	var thisg = element.append("g")
+		var thisg = element.append("g")
 			.attr("class","sidebutton noselect")
 			.attr("id", id)
 	
 		thisg.append("rect")
-		.attr("id", id)
-		.attr("class","sidebutton")
+		//.attr("class","sidebutton")
 		.attr("x", x)
 		.attr("y", y)
 		.attr("width", width)
@@ -76,7 +89,9 @@
 			}
 		})
 
-		document.getElementById(svg_button_id).setAttribute("on", false);
+		d3.select("#"+svg_button_id).attr("on", false)
+			.selectAll("text").attr("opacity",.5);
+		
 
 		var html_button = document.createElement("input")
 		html_button.id = html_button_id;
@@ -87,6 +102,9 @@
 	}
 
 	button_maker.add_radio_button = function(element, id, x, y, r, other_radio_buttons, associated_buttons, unassociated_buttons){
+		
+		
+		
 		this.add_circle_button(element, id + this.radio_base, x, y, r, this.radio_base_color, "", function(){
 			this.setAttribute("selected", true);
 			document.getElementById(id + button_maker.radio_center).setAttribute("fill", button_maker.radio_center_on_color);
@@ -97,13 +115,11 @@
 			}
 
 			for (i = 0; i < associated_buttons.length; i++){
-				document.getElementById(associated_buttons[i]).setAttribute("fill", button_maker.button_on_color);
-				document.getElementById(associated_buttons[i]).setAttribute("on", true);
+				button_maker.activate(d3.select("#"+associated_buttons[i]));
 			}
 
 			for (i = 0; i < unassociated_buttons.length; i++){
-				document.getElementById(unassociated_buttons[i]).setAttribute("fill", button_maker.button_off_color);
-				document.getElementById(unassociated_buttons[i]).setAttribute("on", false);
+				button_maker.deactivate(d3.select("#"+unassociated_buttons[i]));
 			}
 
 		})
@@ -120,16 +136,16 @@
 			}
 
 			for (i = 0; i < associated_buttons.length; i++){
-				document.getElementById(associated_buttons[i]).setAttribute("fill", button_maker.button_on_color);
-				document.getElementById(associated_buttons[i]).setAttribute("on", true);
+				button_maker.activate(d3.select("#"+associated_buttons[i]));
 			}
 
 			for (i = 0; i < unassociated_buttons.length; i++){
-				document.getElementById(unassociated_buttons[i]).setAttribute("fill", button_maker.button_off_color);
-				document.getElementById(unassociated_buttons[i]).setAttribute("on", false);
+				button_maker.deactivate(d3.select("#"+unassociated_buttons[i]));
 			}
 		})
 	}
+	
+
 
 	this.button_maker = button_maker;
 })();
