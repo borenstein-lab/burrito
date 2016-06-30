b99b86a2e324a8e60041e1153b5d58e4b9cb2b01
CECILIA A. NOECKER <cnoecker@elbo-spice.gs.washington.edu>
2016-06-29 17:23:50 -0700

    edge sizes proportional to taxon relative contrib

--- server.R
+++ server.R
@@ -299,6 +299,8 @@ shinyServer(function(input, output, session) {
 			session$sendCustomMessage("upload_status", "Calculating average function abundances")
 
 			otu_matched_samples = grep(".*_comparison$", output$Sample, invert=TRUE)
+			#Make sure to exclude average contributions
+			otu_matched_samples = otu_matched_samples[otu_matched_samples != "Average_contrib"] 
 			#sum over taxa
 			count_name = names(output)[!names(output) %in% c("Sample", "OTU", "SubPathway")]
 			total_funcs = output[otu_matched_samples,lapply(.SD, sum), by=list(SubPathway, Sample), .SDcols=count_name] #whatever the count name is
@@ -442,8 +444,17 @@ shinyServer(function(input, output, session) {
 			# Format the contribution table to match the expected javascript array
 			# Reshape so there's a column for every SubPathway, rows correspond to unique Sample + OTU pairings
 			output[,SubPathway:=paste("SubPathway",SubPathway,sep="_")]
-			output = dcast(output, Sample + OTU ~ SubPathway, value.var="relative_contributions")
+			
+			##add a sample called "Average_contrib" to the contribution table, include 0s for missing OTU/Pathway combos
+			foo = melt(dcast(output, Sample + OTU ~ SubPathway, value.var = "relative_contributions", fun.aggregate=sum), id.vars = c("Sample", "OTU"), variable.name = "SubPathway", value.name = "value")
+			num_samps = output[,length(unique(Sample))]			
+			averages = foo[,sum(value)/num_samps, by=list(OTU, SubPathway)]
 
+			averages[,Sample:="Average_contrib"]
+			setnames(averages, "V1", "relative_contributions")
+			output = rbind(output, averages[,list(Sample, OTU, SubPathway, relative_contributions)])
+			
+			output = dcast(output, Sample + OTU ~ SubPathway, value.var="relative_contributions")
 			# Create a single column for SubPathways that contains a list of the SubPathways for that row's Sample and OTU, remove empty elements from those lists
 			output = data.table(Sample = output$Sample, OTU = output$OTU, SubPathways = apply(output[,3:dim(output)[2], with=FALSE], 1, function(row){return(as.list(row[!is.na(row)]))}))
 
@@ -463,6 +474,7 @@ shinyServer(function(input, output, session) {
 			tracked_data$contribution_table = output
 
 			session$sendCustomMessage("number_of_samples_message", length(output))
+			
 
 			session$sendCustomMessage(type="contribution_table_ready", length(output))
 		}
