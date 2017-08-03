18ebfa7dac3cfb8bde5640d3c74fb01bc8faf9cd
CECILIA A. NOECKER <cnoecker@elbo-spice.gs.washington.edu>
2017-08-03 12:07:11 -0700

    added help message for edges

--- www/Javascript/helpOverlay.js
+++ www/Javascript/helpOverlay.js
@@ -9,6 +9,7 @@
 		helpOverlay.makeHelpLabel("Click to reveal image export menu", "help_sidebar", 0.002, 0.053);	
 		helpOverlay.makeHelpLabel("Click nodes to expand and collapse the phylogenetic tree", "help_taxnode", 0.048, 0.285);
 		helpOverlay.makeHelpLabel("Click bars to lock highlighting", "help_bars", 0.347, 0.11);
+		helpOverlay.makeHelpLabel("Click edges to highlight individual taxon-function links", "help_edges", 0.40, 0.30);
 		helpOverlay.makeHelpLabel("Mouse over bar charts for detailed abundance information", "help_taxbar", 0.099, 0.674);
 		helpOverlay.makeHelpLabel("Click nodes to expand and collapse the functional hierarchy", "help_funcnode", 0.72, 0.23);
 		helpOverlay.makeHelpLabel("Highlight taxa to reveal taxa-specific functional contributions", "help_funcbar", 0.63, 0.65);
