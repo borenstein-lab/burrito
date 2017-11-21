24338baf0f54d90fdadf1f1f8f13f8bd66f1ec29
CECILIA A. NOECKER <cnoecker@elbo-spice.gs.washington.edu>
2017-11-20 17:48:02 -0800

    fixed DA testing to include paired metagenome, added exports to docs

--- docs/export.md
+++ docs/export.md
@@ -21,6 +21,18 @@ Any regions of the visualization highlighted by clicking will also be highlighte
 
 ### Exporting Data Files
 
-The BURRITO server generates summarized tables of function abundances and attributions that are displayed in the visualization. These tables can be downloaded for further analysis. 
+The BURRITO server generates summarized tables of function abundances and attributions that are displayed in the visualization, as well as results of basic summary analyses. These tables can be downloaded for further analysis 
+via the sidebar menu. See [Uploading Data to BURRITO](data_input.html) for more information on the settings and options for these files.
+
+- *Function abundance tables*: 
 Specifically, the estimated function abundances and the taxon-specific function attributions can each be downloaded. These are supplied at the level of resolution selected on the upload page 
 (Genus for taxa and sub-pathway for functions by default). These are formatted the same as output files from PICRUSt (Langille Nature Biotech 2013). 
+
+- *Quality control of functional predictions*: If the provided data uses the default Greengenes OTUs and PICRUSt functional predictions, BURRITO will calculate and 
+provide for download the weighted Nearest Sequenced Taxon Index (NSTI) for each sample, a score indicating the average branch length separating each OTU from a sequenced reference genome (weighted
+by OTU abundances).
+
+- *Basic differential abundance statistics*: If users provide and select a binary sample grouping (for example Cases and Controls), BURRITO will automatically perform and provide preliminary differential abundance calculations
+for all of the displayed taxa and functions, at the user-specified minimum levels of resolution. This includes the difference in mean abundance between the two sample groups and the p-value from a Wilcoxon rank-sum test between the two groups, 
+as well as the adjusted p-value after multiple choice correction based on the Bonferroni correction and the Benjamini-Hochberg false discovery rate methods. If a paired dataset of function abundances is provided, 
+the same statistics will also be reported for those features.
--- server.R
+++ server.R
@@ -1261,10 +1261,10 @@ shinyServer(function(input, output, session) {
 	# If metadata variable is binary, perform Wilcoxon rank-sum tests on all taxa and functions, else return NULL 
 	generate_and_send_statistics_table = function(metadata, otu_table, contribution_table){
 		if(nrow(metadata) > 0 & !is.null(metadata_factor())){
-			#Test for whether metadata_factor() is binary
+			# Test for whether metadata_factor() is binary
 			test_levels = unique(metadata[,get(metadata_factor())])
 			if(length(test_levels) == 2){
-				#For all taxa - Do repeated Wilcoxon rank-sum tests, calculate difference in means
+				# For all taxa - Do repeated Wilcoxon rank-sum tests, calculate difference in means
 				otu_table_test = merge(otu_table, metadata[,c(first_metadata_level(), metadata_factor()),with=F], by=first_metadata_level(), all.x = F, all.y = F) #Only samples that have metadata assignment 
 				test_results = otu_table_test[,wilcox.test(abundance[get(metadata_factor())==test_levels[1]], abundance[get(metadata_factor())==test_levels[2]])$p.value, by=eval(taxonomic_summary_level())]
 				test_results[,BH_FDR_AdjustP:=p.adjust(V1, method="BH")]
@@ -1273,18 +1273,40 @@ shinyServer(function(input, output, session) {
 				col_name = paste0("Mean_", test_levels[1], "_", test_levels[2], "_Diff")
 				test_results = merge(test_results, diff_means, by=taxonomic_summary_level(), all = T)
 				setnames(test_results, c(taxonomic_summary_level(), "V1.x", "V1.y"), c("Feature", "WilcoxP", col_name))	
-				#for all functions - aggregate contributions, do same calculations
-				func_table_test = contribution_table[,sum(contribution, na.rm=T), by=c(first_metadata_level(), function_summary_level())]
-				setnames(func_table_test, "V1", "abundance")
-				func_table_test = merge(func_table_test, metadata[,c(first_metadata_level(), metadata_factor()),with=F], by=first_metadata_level(), all.x = F, all.y = F)
-				func_test_results = func_table_test[,wilcox.test(abundance[get(metadata_factor())==test_levels[1]], abundance[get(metadata_factor())==test_levels[2]])$p.value, by=eval(function_summary_level())]
+				##### For all functions - aggregate contributions, do same calculations
+				func_table_inferred = contribution_table[!grepl("_comparison", get(first_metadata_level()), fixed = T)] #Remove any paired function samples
+				func_table_mg = contribution_table[grepl("_comparison", get(first_metadata_level()), fixed = T)]
+
+				#First for the inferred functions
+				func_table_inferred = func_table_inferred[,sum(contribution, na.rm=T), by=c(first_metadata_level(), function_summary_level())]
+				setnames(func_table_inferred, "V1", "abundance")
+				func_table_inferred = merge(func_table_inferred, metadata[,c(first_metadata_level(), metadata_factor()),with=F], by=first_metadata_level(), all.x = F, all.y = F)
+				func_test_results = func_table_inferred[,wilcox.test(abundance[get(metadata_factor())==test_levels[1]], abundance[get(metadata_factor())==test_levels[2]])$p.value, by=eval(function_summary_level())]
 				func_test_results[,BH_FDR_AdjustP:=p.adjust(V1, method="BH")]
 				func_test_results[,Bonf_AdjustP:=p.adjust(V1, method="bonferroni")]
-				diff_means = func_table_test[,mean(abundance[get(metadata_factor())==test_levels[1]], na.rm=T) - mean(abundance[get(metadata_factor())==test_levels[2]], na.rm=T), by=eval(function_summary_level())]
+				diff_means = func_table_inferred[,mean(abundance[get(metadata_factor())==test_levels[1]], na.rm=T) - mean(abundance[get(metadata_factor())==test_levels[2]], na.rm=T), by=eval(function_summary_level())]
 				func_test_results = merge(func_test_results, diff_means, by=function_summary_level())
-				setnames(func_test_results, c(function_summary_level(), "V1.x", "V1.y"), c("Feature", "WilcoxP", col_name))					
-				#rbind into big table
+				setnames(func_test_results, c(function_summary_level(), "V1.x", "V1.y"), c("Feature", "WilcoxP", col_name))			
+
+				# Rbind into big table
 				test_results = rbind(test_results, func_test_results, fill = T)			
+				
+				#Repeat if there are metagenome samples
+				if(nrow(func_table_mg) > 0){
+					func_table_mg = func_table_mg[,sum(contribution, na.rm=T), by=c(first_metadata_level(), function_summary_level())]
+					setnames(func_table_mg, "V1", "abundance")
+					func_table_mg[,OrigSample:=gsub("_comparison", "", get(first_metadata_level()))]
+					func_table_mg = merge(func_table_mg, metadata[,c(first_metadata_level(), metadata_factor()),with=F], by.x = "OrigSample", by.y=first_metadata_level(), all.x = F, all.y = F)
+					func_mg_test_results = func_table_mg[,wilcox.test(abundance[get(metadata_factor())==test_levels[1]], abundance[get(metadata_factor())==test_levels[2]])$p.value, by=eval(function_summary_level())]
+					func_mg_test_results[,BH_FDR_AdjustP:=p.adjust(V1, method="BH")]
+					func_mg_test_results[,Bonf_AdjustP:=p.adjust(V1, method="bonferroni")]
+					diff_means = func_table_mg[,mean(abundance[get(metadata_factor())==test_levels[1]], na.rm=T) - mean(abundance[get(metadata_factor())==test_levels[2]], na.rm=T), by=eval(function_summary_level())]
+					func_mg_test_results = merge(func_mg_test_results, diff_means, by=function_summary_level())
+					setnames(func_mg_test_results, c(function_summary_level(), "V1.x", "V1.y"), c("Feature", "WilcoxP", col_name))
+					func_mg_test_results[,Feature:=paste0("Metagenome_", Feature)]
+					test_results = rbind(test_results, func_mg_test_results, fill = T)			
+				}			
+								
 				test_results = test_results[,c("Feature", col_name, "WilcoxP", "BH_FDR_AdjustP", "Bonf_AdjustP"), with=F]
 				session$sendCustomMessage("statistics_table", test_results)
 			} else {
